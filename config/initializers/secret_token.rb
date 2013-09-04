b7f9f83ca41b3348a0f30302ae11ccabafaab8da
lordazl1n <lordazl1n@gmail.com>
2013-09-05 02:55:16 +0700

    fuckedup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fuckedup::Application.config.secret_key_base = 'a8fea75ab2815203df82d7e260d0aead825a59678dd3332be17b8d3eabc636cdca51171f3f6c0068f81fb2882109eee05ae7cbf8a46a326355a7c2b8e5244ea7'
